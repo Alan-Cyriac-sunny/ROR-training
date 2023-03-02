65941e030fe2155a99700834ea60176a07b2fa59
Alan-chordify <alan.sunny@chordify.com>
2023-03-02 22:51:32 +0530

    changes to vote.rb and book_details.rb

--- book_details.rb
+++ book_details.rb
@@ -3,16 +3,52 @@
 # to search for a book by its name and update its year of publication.Help Mike write 
 # a Ruby program to solve this problem using hash operations.
 
-book_details = {
-	"A Better India: A Better World" <=	Narayana Murthy
-	"A Passage to India" 			 <= E.M. Foster
-	"A Revenue Stamp" 				 <= Amrita Pritam
-	"Death of a City	Amrita Pritam" <=
-	"Pinjar	Amrita Pritam" <=
-	"A Suitable Boy	Vikram Seth" <=
-	"A Tale of Two Cities	Charles Darwin" <=
-	"David Copperfield	Charles Darwin" <=
-	"Oliver Twist	Charles Darwin" <=
-	"Origin of Species	Charles Darwin" <=
-	"A Week with Gandhi	Louis Fisher" <=
-}
+book_details = [
+	{:name => "A Better India; A Better World", :author => "Narayana Murthy", :yop => nil },
+	{:name => "A Passage to India", :author => "E.M. Foster", :yop => nil},
+	{:name => "A Revenue Stamp", :author=> "Amrita Pritam", :yop => nil},
+	{:name => "Death of a City", :author=> "Amrita Pritam", :yop => nil},
+	{:name =>"Pinjar Amrita", :author=> "Pritam", :yop => nil},
+	{:name => "A Suitable Boy", :author=> "Vikram Seth", :yop => nil},
+	{:name => "A Tale of Two Cities", :author=>	"Charles Darwin", :yop => nil},
+	{:name => "Oliver Twist", :author=> "Charles Darwin", :yop => nil},
+	{:name => "David Copperfield", :author=> "Charles Darwin", :yop => nil},
+	{:name => "Oliver Twist", :author=> "Charles Darwin", :yop => nil},
+	{:name => "Origin of Species", :author=> "Charles Darwin", :yop => nil},
+	{:name => "A Week with Gandhi", :author=> "Louis Fisher", :yop => nil} 
+]
+
+while true
+	puts "enter book name"
+	book_name = gets.chomp
+	choice1 = choice2 =  0
+	flag =0
+
+	book_details.each do |book|
+		if book[:name].downcase == book_name.downcase
+			flag = 1
+			puts "book found. \n do you want to update the year of publication \nchoose [1/0]"
+			choice1 = gets.chomp.to_i
+	
+			if choice1 == 1
+				puts "enter year of publication"
+				year = gets.chomp.to_i
+				book[:yop] = year
+				puts "updated record \n#{book}"
+				break
+			else
+				if flag==0
+					puts "book not found."
+				end
+				break
+			end
+		end
+	end
+	puts "\n do you want to continue in the application \nchoose [1/0]"
+	choice2 = gets.chomp.to_i
+	if choice2 == 0
+		exit 1
+	end
+end
+
+
--- vote.rb
+++ vote.rb
@@ -1,3 +1,8 @@
+# You are creating a program that determines if a person is eligible to vote in a particular election. 
+# In order to vote, a person must be at least 18 years old and a citizen of the country in which the 
+# election is taking place. Write a Ruby program that takes a person's age and citizenship status as
+#  input and outputs whether they are eligible to vote
+
 puts "enter the country which the election is held"
 country = gets.chomp
 puts "enter your age: "
@@ -5,6 +10,7 @@ age = gets.chomp.to_i
 puts "are you a citizen of #{country} [y/n]: "
 sts = gets.chomp
 sts=sts.downcase
+
 if (age>17)
 	if(sts == "y")
 		puts "you are eligible to vote"
