2f2bb41158aef7bc7bb2611e5148fb880df06366
Alan-chordify <alan.sunny@chordify.com>
2023-03-11 23:13:36 +0530

    oops2 modification

--- OOPs_2.rb
+++ OOPs_2.rb
@@ -1,44 +1,43 @@
-# frozen_string_literal: true
-
-# Problem: Alice is a programmer who is working on a project that involves
-# creating a new class called Vehicle. The Vehicle class has several public
-# methods, such as start_engine and stop_engine. Alice's colleague, Bob, is
-# working on a subclass of Vehicle called Car. Car inherits all of the public
-# methods from Vehicle, but Bob needs to add some new functionality to the
-# start_engine method. How can Bob access the start_engine method in the
+# Problem: Alice is a programmer who is working on a project that involves 
+# creating a new class called Vehicle. The Vehicle class has several public 
+# methods, such as start_engine and stop_engine. Alice's colleague, Bob, is 
+# working on a subclass of Vehicle called Car. Car inherits all of the public 
+# methods from Vehicle, but Bob needs to add some new functionality to the 
+# start_engine method. How can Bob access the start_engine method in the 
 # Vehicle class so he can use it in his Car subclass?
 
 class Vehicle
-  def initialize
-    startEngine
-    stopEngine
-  end
+	def initialize
+		self.startEngine
+		self.stopEngine
+	end 
 
-  def startEngine
-    puts 'Engine Running'
-  end
+    def startEngine
+        puts "Engine Running"
+    end
 
-  def stopEngine
-    puts 'Engine stopped'
-  end
+    def stopEngine
+        puts "Engine stopped"
+    end
 end
 
 class Car < Vehicle
-  def initialize
-    startEngine
-    stopEngine
-  end
+	def initialize
+		self.startEngine
+		self.stopEngine
+	end
+	
+    def startEngine
+        super
+        puts "Music On"
+    end
 
-  def startEngine
-    super
-    puts 'Music On'
-  end
-
-  def stopEngine
-    super
-    puts 'Music Off'
-  end
+    def stopEngine
+    	super
+        puts "Music Off"
+    end
 end
+
 
 bike = Vehicle.new
 car = Car.new
